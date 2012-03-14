b04c67c1892925718b4c5b231ec39bcdc78b19e8
Ankur Oberoi <aoberoi@gmail.com>
2012-03-14 18:57:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkngo::Application.config.secret_token = 'a592ea13f720dd8fc744e3738111ff4fdc3a67cb158edad1ce53580c5c0914dfd9fc33cd20d7c254176de15da17be4141616554d73cb2b5bf33b0abb2859ad92'
